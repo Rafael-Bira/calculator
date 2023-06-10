74affca782076f934e5ce44b6d693f7f310a64dc
Rafael-Bira <rafaelmichelinsgmail.com>
2023-06-10 19:40:39 -0300

    Changed some styles and added 'Math Error' message when dividing by zero.

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ function deleteLastChar() {
     operation.value = operation.value.slice(0, operation.value.length - 1);
 }
 
+function answer() {
+    const answer = document.querySelector("#display").value;
+    document.querySelector("[name=operation]").value = answer;
+}
+
 function calculate() {
     const operation = document.querySelector("[name=operation]").value;
     const display = document.querySelector("#display");
     let result = eval(operation);
+
+// If result has too many decimals, fix to ten
     if(result.toString().length > 10) {
         result = result.toFixed(10);
     }
-    display.value = result;
-}
 
-function answer() {
-    const answer = document.querySelector("#display").value;
-    document.querySelector("[name=operation]").value = answer;
+// Error when dividing by zero
+    if(operation.includes("/0")) {
+        result = result.toString()
+        result = "Math Error: Cannot divide by 0";
+    }
+
+    display.value = result;
 }
