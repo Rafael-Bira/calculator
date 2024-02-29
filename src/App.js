036af78f923d022702670d84dce6b5f4a863388b
Rafael-Bira <rafaelmichelinsgmail.com>
2024-02-29 19:31:48 -0300

    Added name attribute for every button

--- src/App.js
+++ src/App.js
@@ -38,46 +38,46 @@ const App = () => {
 
             <div id="calculator">
                 <div>
-                    <input type="text" className="operation" value={input} readOnly />
+                    <input type="text" name="expression" className="operation" value={input} readOnly />
                     <br />
-                    <button className="calculate" onClick={() => handleInput("=")}>=</button>
+                    <button name="=" className="calculate" onClick={() => handleInput("=")}>=</button>
                     <br />
-                    <input type="text" className="result" value={result} readOnly />
+                    <input type="text" name="result" className="result" value={result} readOnly />
                 </div>
 
                 <div>
-                    <button className="other" onClick={() => handleInput("Ans")}>Ans</button>
-                    <button className="other" onClick={() => handleInput("Del")}>Del</button>
-                    <button className="other" onClick={() => handleInput("(")}>(</button>
-                    <button className="other" onClick={() => handleInput(")")}>)</button>
+                    <button name="ans" className="other" onClick={() => handleInput("Ans")}>Ans</button>
+                    <button name="del" className="other" onClick={() => handleInput("Del")}>Del</button>
+                    <button name="(" className="other" onClick={() => handleInput("(")}>(</button>
+                    <button name=")" className="other" onClick={() => handleInput(")")}>)</button>
                 </div>
 
                 <div>
-                    <button className="number" onClick={() => handleInput("7")}>7</button>
-                    <button className="number" onClick={() => handleInput("8")}>8</button>
-                    <button className="number" onClick={() => handleInput("9")}>9</button>
-                    <button className="operator" onClick={() => handleInput("+")}>+</button>
+                    <button name="7" className="number" onClick={() => handleInput("7")}>7</button>
+                    <button name="8" className="number" onClick={() => handleInput("8")}>8</button>
+                    <button name="9" className="number" onClick={() => handleInput("9")}>9</button>
+                    <button name="+" className="operator" onClick={() => handleInput("+")}>+</button>
                 </div>
                 
                 <div>
-                    <button className="number" onClick={() => handleInput("4")}>4</button>
-                    <button className="number" onClick={() => handleInput("5")}>5</button>
-                    <button className="number" onClick={() => handleInput("6")}>6</button>
-                    <button className="operator" onClick={() => handleInput("-")}>-</button>
+                    <button name="4" className="number" onClick={() => handleInput("4")}>4</button>
+                    <button name="5" className="number" onClick={() => handleInput("5")}>5</button>
+                    <button name="6" className="number" onClick={() => handleInput("6")}>6</button>
+                    <button name="-" className="operator" onClick={() => handleInput("-")}>-</button>
                 </div>
                 
                 <div>
-                    <button className="number" onClick={() => handleInput("1")}>1</button>
-                    <button className="number" onClick={() => handleInput("2")}>2</button>
-                    <button className="number" onClick={() => handleInput("3")}>3</button>
-                    <button className="operator" onClick={() => handleInput("*")}>*</button>
+                    <button name="1" className="number" onClick={() => handleInput("1")}>1</button>
+                    <button name="2" className="number" onClick={() => handleInput("2")}>2</button>
+                    <button name="3" className="number" onClick={() => handleInput("3")}>3</button>
+                    <button name="*" className="operator" onClick={() => handleInput("*")}>*</button>
                 </div>
                 
                 <div>
-                    <button className="number" onClick={() => handleInput("0")}>0</button>
-                    <button className="other" onClick={() => handleInput(".")}>.</button>
-                    <button className="other" onClick={() => handleInput("C")}>C</button>
-                    <button className="operator" onClick={() => handleInput("/")}>/</button>
+                    <button name="0" className="number" onClick={() => handleInput("0")}>0</button>
+                    <button name="." className="other" onClick={() => handleInput(".")}>.</button>
+                    <button name="C" className="other" onClick={() => handleInput("C")}>C</button>
+                    <button name="/" className="operator" onClick={() => handleInput("/")}>/</button>
                 </div>
             </div>
         </>
